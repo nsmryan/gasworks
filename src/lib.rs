9567e10876a3e53259c6cdae8d73dc4218c1a87c
nsmryan <nsmryan@gmail.com>
2018-09-06 23:02:44 -0400

    decoding prim now compiles. commented out some code that I don't understand how to fix

--- src/lib.rs
+++ src/lib.rs
@@ -2,104 +2,118 @@
 use std::collections::HashSet;
 use std::collections::HashMap;
 
-use bytes::*;
+extern crate bytes;
+use bytes::{Bytes, Buf};
+
+extern crate byteorder;
+use byteorder::{LittleEndian, BigEndian, ByteOrder};
+
+use std::io::{Cursor, Read};
 
 mod types;
 use types::*;
 
 
-fn decode(layout : Layout, bytes : Bytes) -> ValueMap {
-    let mut map = HashMap::new();
+//fn decode(layout : Layout, bytes : Bytes) -> ValueMap {
+//    let mut map = HashMap::new();
+//
+//    let _ = decode_at(&layout, &bytes, 0, &mut map);
+//
+//    map
+//}
 
-    let _ = decode_at(&layout, &bytes, 0, &mut map);
-
-    map
-}
-
-fn decode_prim(prim : Prim, bytes : Bytes, loc : Loc) -> Value {
+fn decode_prim(prim : Prim, bytes : &mut Cursor<&[u8]>, loc : Loc) -> Value {
     match prim {
-        Int(int_prim) => {
-            decode_int(int_prim, bytes, loc)
+        Prim::Int(int_prim) => {
+            decode_int(int_prim, bytes)
         },
 
-        Float(floatType) => {
+        Prim::Float(floatType) => {
             match floatType {
-                F32(endianness) => {
+                FloatPrim::F32(endianness) => {
                     match endianness {
-                        BigEndian   => BigEndian::read_f32(bytes.take(std::mem::size_of::<f32>())),
-                        LitleEndian => LittleEndian::read_f32(bytes.take(std::mem::size_of::<f32>())),
-                    },
+                        Endianness::BigEndian    => Value::F32(bytes.get_f32_be()),
+                        Endianness::LittleEndian => Value::F32(bytes.get_f32_le()),
+                    }
+                }
 
-                F64(endianness) => {
+                FloatPrim::F64(endianness) => {
                     match endianness {
-                        BigEndian   => BigEndian::read_f64(bytes.take(std::mem::size_of::<f64>())),
-                        LitleEndian => LittleEndian::read_f64(bytes.take(std::mem::size_of::<f64>())),
-                    },
+                        Endianness::BigEndian    => Value::F64(bytes.get_f64_be()),
+                        Endianness::LittleEndian => Value::F64(bytes.get_f64_le()),
+                    }
+                },
             }
         },
 
-        Bytes(num_bytes) => {
-            Value::Bytes(bytes.take(num_bytes))
-        },
+        // NOTE add bytes back in when you understand how to avoid copying
+        //Prim::Bytes(num_bytes) => {
+        //    let buf = vec![0; num_bytes];
+        //    
+        //    bytes.read_exact(buf.as_mut_slice()).unwrap();
 
-        Bits(num_bytes) => {
+        //    Value::Bytes(buf.as_slice())
+        //},
+
+        Prim::Bits(num_bytes) => {
             unimplemented!()
         },
 
-        Enum(Enum{map, int_prim}) => {
+        Prim::Enum(Enum{map, int_prim}) => {
             // NOTE this doesn't ensure that the int_prim
             // decodes to a value in the map, and doesn't
             // enforce that the int_value.value doesn't loose precision
             let int_value = decode_int(int_prim, bytes);
-            let int = int_value.value;
-            Value::Enum(map.get(int)?, int)
+            let int = int_value.value();
+            // NOTE the use of to_string here may be wrong?
+            Value::Enum(map.get(&int).unwrap().to_string(), int)
         },
     }
 }
 
-fn decode_int(int_prim, bytes) -> Value {
-    let Int(IntPrim{size, signedness, endianness}) = int_prim;
+fn decode_int(int_prim : IntPrim, bytes : &mut Cursor<&[u8]>) -> Value {
+    let IntPrim{size, signedness, endianness} = int_prim;
 
     match endianness {
-        BigEndian => {
+        Endianness::BigEndian => {
             match signedness {
-                Unsigned => {
+                Signedness::Unsigned => {
                     match size {
-                        Bits8  => Value::U8(bytes.get_u8()),
-                        Bits16 => Value::U16(bytes.get_u16()),
-                        Bits32 => Value::U32(bytes.get_u32()),
-                        Bits64 => Value::U64(bytes.get_u64()),
+                        IntSize::Bits8  => Value::U8(bytes.get_u8()),
+                        IntSize::Bits16 => Value::U16(bytes.get_u16_be()),
+                        IntSize::Bits32 => Value::U32(bytes.get_u32_be()),
+                        IntSize::Bits64 => Value::U64(bytes.get_u64_be()),
                     }
                 },
 
-                Signed => {
+                Signedness::Signed => {
                     match size {
-                        Bits8  => Value::I8(bytes.get_i8()),
-                        Bits16 => Value::I16(bytes.get_i16()),
-                        Bits32 => Value::I32(bytes.get_i32()),
-                        Bits64 => Value::I64(bytes.get_i64()),
+                        IntSize::Bits8  => Value::I8(bytes.get_i8()),
+                        IntSize::Bits16 => Value::I16(bytes.get_i16_be()),
+                        IntSize::Bits32 => Value::I32(bytes.get_i32_be()),
+                        IntSize::Bits64 => Value::I64(bytes.get_i64_be()),
                     }
                 },
             }
         }
 
-        LittleEndian => {
+        Endianness::LittleEndian => {
             match signedness {
-                Unsigned => {
+                Signedness::Unsigned => {
                     match size {
-                        Bits8  => Value::I8(bytes.get_u8()),
-                        Bits16 => Value::I16(bytes.get_u16_le()),
-                        Bits32 => Value::I32(bytes.get_u32_le()),
-                        Bits64 => Value::I64(bytes.get_u64_le()),
+                        IntSize::Bits8  => Value::U8(bytes.get_u8()),
+                        IntSize::Bits16 => Value::U16(bytes.get_u16_be()),
+                        IntSize::Bits32 => Value::U32(bytes.get_u32_be()),
+                        IntSize::Bits64 => Value::U64(bytes.get_u64_be()),
                     }
                 },
 
-                Signed => {
+                Signedness::Signed => {
                     match size {
-                        Bits8  => Value::I8(bytes.get_i8()),
-                        Bits16 => Value::I16(bytes.get_i16_le()),
-                        Bits32 => Value::I32(bytes.get_i32_le()),
-                        Bits64 => Value::I64(bytes.get_i64_le()),
+                        IntSize::Bits8  => Value::I8(bytes.get_i8()),
+                        IntSize::Bits16 => Value::I16(bytes.get_i16_le()),
+                        IntSize::Bits32 => Value::I32(bytes.get_i32_le()),
+                        IntSize::Bits64 => Value::I64(bytes.get_i64_le()),
                     }
                 },
             }
@@ -107,35 +121,36 @@ fn decode_int(int_prim, bytes) -> Value {
     }
 }
 
-fn decode_at(layout : &Layout, bytes : Bytes, loc : Loc, map : &mut ValueMap) -> Loc {
-    use types::Layout::*;
-    match layout {
-        Prim(prim) => {
-            decode_prim(prim, bytes, loc);
-        },
-
-        Seq(layouts) => {
-            let mut loc = loc;
-            for layout in layouts.iter() {
-                let new_loc = decode_at(layout, bytes, loc, map);
-                loc = new_loc;
-            }
-
-            loc
-        },
-
-        All(layouts) => {
-            let mut max_loc = loc;
-            for layout in layouts.iter() {
-                let new_loc = decode_at(layout, bytes, loc, map);
-                if new_loc > max_loc {
-                    max_loc = new_loc;
-                }
-            }
-
-            max_loc
-        },
-    }
-}
+// NOTE add decode_at back in, resulting in a HashMap<Name, Value>
+//fn decode_at(layout : &Layout, bytes : Bytes, loc : Loc, map : &mut ValueMap) -> Value {
+//    use types::Layout::*;
+//    match layout {
+//        Prim(prim) => {
+//            decode_prim(prim, bytes, loc)
+//        },
+//
+//        Seq(layouts) => {
+//            let mut loc = loc;
+//            for layout in layouts.iter() {
+//                let new_loc = decode_at(layout, bytes, loc, map);
+//                loc = new_loc;
+//            }
+//
+//            loc
+//        },
+//
+//        All(layouts) => {
+//            let mut max_loc = loc;
+//            for layout in layouts.iter() {
+//                let new_loc = decode_at(layout, bytes, loc, map);
+//                if new_loc > max_loc {
+//                    max_loc = new_loc;
+//                }
+//            }
+//
+//            max_loc
+//        },
+//    }
+//}
 
 
--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,9 @@
 use std::collections::HashSet;
 use std::collections::HashMap;
 
+extern crate bytes;
+use self::bytes::{Bytes, Buf};
+
 pub type Name = String;
 
 pub enum Endianness {
@@ -37,14 +40,14 @@ pub struct BitPrim {
 }
 
 pub struct Enum {
-    map : HashMap<i64, Name>,
-    int_prim : IntPrim,
+    pub map : HashMap<i64, Name>,
+    pub int_prim : IntPrim,
 }
 
 pub enum Prim {
     Int(IntPrim),
     Float(FloatPrim),
-    Bytes(u64),
+    //Bytes(usize),
     Bits(BitPrim),
     Enum(Enum),
 }
@@ -74,7 +77,7 @@ pub enum Protocol {
     Packet(Packet),
 }
 
-pub type Loc = u64;
+pub type Loc = usize;
 
 pub type LayoutMap = HashMap<Name, (Loc, Prim)>;
 
@@ -89,7 +92,7 @@ pub enum Value {
     I64(i64),
     F32(f32),
     F64(f64),
-    Bytes(Bytes),
+    //Bytes(&[u8]),
     Enum(Name, i64),
 }
 
@@ -104,20 +107,20 @@ pub type ValueMap = HashMap<Name, Value>;
 impl Value {
     // NOTE this would work better with an IntValue separate
     // from the Value type
-    fn value(&self) -> i64 {
+    pub fn value(&self) -> i64 {
         match self {
-            U8(int)  =>   int as i64,
-            U16(int) =>   int as i64,
-            U32(int) =>   int as i64,
-            U64(int) =>   int as i64,
-            I8(int)  =>   int as i64,
-            I16(int) =>   int as i64,
-            I32(int) =>   int as i64,
-            I64(int) =>   int as i64,
-            F32(int) =>   panic!("Found an F32 in a value, expecting an int!"),
-            F64(int) =>   panic!("Found an F64 in a value, expecting an int!"),
-            Bytes(_) =>   panic!("Found an Bytes in a value, expecting an int!"),
-            Enum(_, _) => panic!("Found an Enum in a value, expecting an int!"),
+            Value::U8(int)  =>   *int as i64,
+            Value::U16(int) =>   *int as i64,
+            Value::U32(int) =>   *int as i64,
+            Value::U64(int) =>   *int as i64,
+            Value::I8(int)  =>   *int as i64,
+            Value::I16(int) =>   *int as i64,
+            Value::I32(int) =>   *int as i64,
+            Value::I64(int) =>   *int as i64,
+            Value::F32(int) =>   panic!("Found an F32 in a value, expecting an int!"),
+            Value::F64(int) =>   panic!("Found an F64 in a value, expecting an int!"),
+            //Value::Bytes(_) =>   panic!("Found an Bytes in a value, expecting an int!"),
+            Value::Enum(_, _) => panic!("Found an Enum in a value, expecting an int!"),
         }
     }
 }
